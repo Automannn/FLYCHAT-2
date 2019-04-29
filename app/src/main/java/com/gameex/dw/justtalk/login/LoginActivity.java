70927dfa6aa9a521b5b4ea74fb1598e61e687db6
Dennil_Wei <greacezli@outlook.com>
2019-04-29 17:53:31 +0800

    2019-4-27:按照极光IM Android集成文档，在AndroidManifest.xml中增加一些权限，新建一个service和一些receiver，未测试 2019-4-29:集成JPush，可进行简单单聊文本信息接收与发送（ps：目前只有两个用户），可从服务器进行消息的推送和通知

--- app/src/main/java/com/gameex/dw/justtalk/login/LoginActivity.java
+++ app/src/main/java/com/gameex/dw/justtalk/login/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.gameex.dw.justtalk.login;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,13 +28,32 @@ import com.gameex.dw.justtalk.dataStream.GetUserService;
 import com.gameex.dw.justtalk.managePack.BaseActivity;
 import com.gameex.dw.justtalk.signUp.SignUpActivity;
 import com.gameex.dw.justtalk.util.BarUtil;
+import com.gameex.dw.justtalk.util.DataUtil;
+import com.gameex.dw.justtalk.util.LogUtil;
 import com.github.siyamed.shapeimageview.CircularImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.api.BasicCallback;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * 登录activity
  */
 public class LoginActivity extends BaseActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
-
+    private static final String LOGIN_URL = "http://117.50.57.86:8060/user/login";
+    private static final int SIGN_UP_REQUEST_CODE = 201;
 
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
@@ -39,6 +65,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     private TextView mQuickSign, mForgotPwd;
 
     private Intent mIntentGetUser;
+    private ProgressDialog prosDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,10 +105,12 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
         super.onResume();
         if (getIntent().getExtras() == null) {
             inputLogin();
-            checkToLogin();
         } else {
             if (getIntent().getStringExtra("flag").equals("LoginOut")) {
-                inputLogin();
+                pref = PreferenceManager.getDefaultSharedPreferences(this);
+                String account = pref.getString("account", "");
+                mUsername.setText(account);
+                mAutoLogin.setChecked(true);
                 editor = pref.edit();
                 editor.remove("account");
                 editor.remove("password");
@@ -104,41 +133,147 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
             mUsername.setText(account);
             mPassword.setText(password);
             mAutoLogin.setChecked(true);
+            if (!TextUtils.isEmpty(account) && !TextUtils.isEmpty(password)) {
+                Intent intentLogin = new Intent(LoginActivity.this, BottomBarActivity.class);
+                finish();
+                startActivity(intentLogin);
+            } else {
+                checkToLogin(0);
+            }
         }
     }
 
     /**
      * 验证登录
      */
-    private void checkToLogin() {
-        if (!mUsername.getText().toString().isEmpty()) {
-            String account = mUsername.getText().toString();
-            String password = mPassword.getText().toString();
-            if (mUsername.getText().toString().equals("小飞侠")) {
-                if (mPassword.getText().toString().equals("flyxia.cn")) {
-                    editor = pref.edit();
-                    if (mAutoLogin.isChecked()) {
-                        editor.putBoolean("remember_password", true);
-                        editor.putString("account", account);
-                        editor.putString("password", password);
-                    } else {
-                        editor.clear();
-                    }
-                    editor.apply();
-                    Intent intentLogin = new Intent(this, BottomBarActivity.class);
-                    startActivity(intentLogin);
-                    finish();
-                } else {
-                    YoYo.with(Techniques.Shake)
-                            .duration(500)
-                            .playOn(findViewById(R.id.password_layout));
-                    Toast.makeText(this, "密码错误", Toast.LENGTH_SHORT).show();
+    private void checkToLogin(int flag) {
+        if (flag == -1) {
+            JMessageClient.login("18180027763", "123456dw", new BasicCallback() {
+                @Override
+                public void gotResult(int responseCode, String registerDesc) {
+                    LogUtil.i("LOGIN_ACTIVITY_LMESSAGE_LOGIN",
+                            "JMessageClient.register " +
+                                    ", responseCode = " + responseCode +
+                                    " ; registerDesc = " + registerDesc);
                 }
+            });
+            Intent intentLogin = new Intent(LoginActivity.this, BottomBarActivity.class);
+            finish();
+            startActivity(intentLogin);
+        }
+        if (!TextUtils.isEmpty(mUsername.getText())) {
+            if (!TextUtils.isEmpty(mPassword.getText())) {
+                prosDialog = new ProgressDialog(this);
+                prosDialog.setTitle("提示");
+                prosDialog.setMessage("正在登录...");
+                prosDialog.setCancelable(false);
+                prosDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                prosDialog.show();
+                final String account = mUsername.getText().toString();
+                final String password = mPassword.getText().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MediaType JSON = MediaType.parse("application/json;charset=utf-8");
+                        JSONObject json = new JSONObject();
+                        try {
+                            if (DataUtil.isMobileNumber(account)) {
+                                json.put("phoneNumber", account);
+                            } else {
+                                json.put("username", account);
+                            }
+                            json.put("password", password);
+                            OkHttpClient client = new OkHttpClient();
+                            RequestBody requestBody = RequestBody.create(JSON, String.valueOf(json));
+                            Request request = new Request.Builder()
+                                    .url(LOGIN_URL)
+                                    .post(requestBody)
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            if (response.isSuccessful()) {
+                                assert response.body() != null;
+                                String string = response.body().string();
+                                LogUtil.d("RESPONSE_STRING", string);
+                                JSONObject jsonObject = new JSONObject(string);
+                                boolean isSuccess = jsonObject.getBoolean("success");
+                                if (isSuccess) {
+                                    prosDialog.dismiss();
+                                    editor = pref.edit();
+                                    if (mAutoLogin.isChecked()) {
+                                        editor.putBoolean("remember_password", true);
+                                        editor.putString("account", account);
+                                        editor.putString("password", password);
+                                    } else {
+                                        editor.clear();
+                                    }
+                                    editor.apply();
+                                    JMessageClient.login(account, password, new BasicCallback() {
+                                        @Override
+                                        public void gotResult(int responseCode, String registerDesc) {
+                                            LogUtil.i("LOGIN_ACTIVITY_LMESSAGE_LOGIN", "JMessageClient.register " + ", responseCode = " + responseCode + " ; registerDesc = " + registerDesc);
+                                        }
+                                    });
+                                    Intent intentLogin = new Intent(LoginActivity.this, BottomBarActivity.class);
+                                    finish();
+                                    startActivity(intentLogin);
+                                } else {
+                                    prosDialog.dismiss();
+                                    JSONObject object = jsonObject.getJSONObject("data");
+                                    int code = object.getInt("code");
+                                    String message = object.getString("message");
+                                    if (code == 403) {
+                                        Looper.prepare();
+                                        YoYo.with(Techniques.Shake)
+                                                .duration(700)
+                                                .playOn(findViewById(R.id.username_layout));
+                                        Toast.makeText(LoginActivity.this, message + "", Toast.LENGTH_SHORT).show();
+                                        Looper.loop();
+                                    } else if (code == 404) {
+                                        Looper.prepare();
+                                        YoYo.with(Techniques.Shake)
+                                                .duration(700)
+                                                .playOn(findViewById(R.id.password_layout));
+                                        Toast.makeText(LoginActivity.this, message + "", Toast.LENGTH_SHORT).show();
+                                        Looper.loop();
+                                    } else {
+                                        Looper.prepare();
+                                        Toast.makeText(LoginActivity.this, "请完善登陆信息", Toast.LENGTH_SHORT).show();
+                                        Looper.loop();
+                                    }
+                                }
+                            } else {
+                                prosDialog.dismiss();
+                                LogUtil.d("RESPONSE_BODY", "null");
+                            }
+                        } catch (IOException e) {
+                            prosDialog.dismiss();
+                            Looper.prepare();
+                            Toast.makeText(LoginActivity.this, "网络异常", Toast.LENGTH_SHORT).show();
+                            Looper.loop();
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            prosDialog.dismiss();
+                            Looper.prepare();
+                            Toast.makeText(LoginActivity.this, "数据异常", Toast.LENGTH_SHORT).show();
+                            Looper.loop();
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
             } else {
+                if (flag == 1) {
+                    YoYo.with(Techniques.Shake)
+                            .duration(700)
+                            .playOn(findViewById(R.id.password_layout));
+                    Toast.makeText(LoginActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            if (flag == 1) {
                 YoYo.with(Techniques.Shake)
                         .duration(700)
                         .playOn(findViewById(R.id.username_layout));
-                Toast.makeText(this, "用户名不存在", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "用户名不能为空", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -147,12 +282,16 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.login_btn:
-                startService(mIntentGetUser);
-                checkToLogin();
+                checkToLogin(1);
+//                checkToLogin(-1);
                 break;
             case R.id.quick_sign_up:
                 Intent intentQuickSign = new Intent(this, SignUpActivity.class);
-                startActivity(intentQuickSign);
+                String mobile = mUsername.getText().toString();
+                if (DataUtil.isMobileNumber(mobile)) {
+                    intentQuickSign.putExtra("phone", mobile);
+                }
+                startActivityForResult(intentQuickSign, SIGN_UP_REQUEST_CODE);
                 break;
             case R.id.forgot_pwd:
                 Toast.makeText(this, "找回密码", Toast.LENGTH_SHORT).show();
@@ -172,8 +311,33 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == SIGN_UP_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                String phone = data.getStringExtra("phone");
+                mUsername.setText(phone);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        stopService(mIntentGetUser);
+//        stopService(mIntentGetUser);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (prosDialog.isShowing()) {
+                prosDialog.dismiss();
+                LogUtil.d("KEYCODE_BACK", "dismiss dialog");
+            } else {
+                LoginActivity.this.finish();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
